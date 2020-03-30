dfd4f4f220420003a55877b1a611537c9d9991f2
sash04ek <a.ovcharov@gmail.com>
2020-03-30 15:21:51 +0300

    Add restriction by IP for superadmin role

--- Module.php
+++ Module.php
@@ -26,6 +26,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 	{
 		$this->subscribeEvent('Login', array($this, 'onLogin'), 10);
 		$this->subscribeEvent('CheckAccountExists', array($this, 'onCheckAccountExists'));
+		$this->subscribeEvent('System::RunEntry::before', array($this, 'onBeforeRunEntry'));
 	}
 	
 	/**
@@ -74,6 +75,12 @@ class Module extends \Aurora\System\Module\AbstractModule
 		{
 			if ($bCorrectEmptyPass || $bCorrectPass)
 			{
+				if (!$this->isClientIpInWhitelist())
+				{
+					$mResult = false;
+					return true;
+				}
+
 				$mResult = array(
 					'token' => 'admin',
 					'id' => '-1'
@@ -84,6 +91,51 @@ class Module extends \Aurora\System\Module\AbstractModule
 				$mResult = false;
 			}
 			return true;
+		}
+	}
+
+	protected function isClientIpInWhitelist()
+	{
+		$mResult = false;
+
+		$aWhitelistIp = $this->getConfig('SuperadminWhitelistIp', []);
+		$ip = \Aurora\System\Utils::getClientIp();
+					
+		if (!empty($ip) && count($aWhitelistIp) > 0 && in_array($ip, $aWhitelistIp))
+		{
+			$mResult = true;
+		}
+
+		return $mResult;
+	}
+
+	public function onBeforeRunEntry(&$aArgs, &$mResult)
+	{
+		$oAuthenticatedUser = \Aurora\System\Api::getAuthenticatedUser();
+		if ($oAuthenticatedUser instanceof \Aurora\Modules\Core\Classes\User && 
+			$oAuthenticatedUser->Role === \Aurora\System\Enums\UserRole::SuperAdmin && !$this->isClientIpInWhitelist())
+		{
+			if (isset($aArgs['EntryName']) && strtolower($aArgs['EntryName']) === 'default')
+			{
+				\Aurora\Modules\Core\Module::Decorator()->Logout();
+			}
+			else
+			{
+				$mResult = \Aurora\System\Managers\Response::GetJsonFromObject(
+					'Json', 
+					\Aurora\System\Managers\Response::ExceptionResponse(
+						'RunEntry', 
+						new \Aurora\System\Exceptions\ApiException(
+							\Aurora\System\Notifications::AccessDenied,
+							null,
+							$this->i18N('ERROR_USER_ACCESS_DENIED'),
+							[], 
+							$this
+						)	
+					)
+				);
+				return true;
+			}
 		}
 	}
 	/***** private functions *****/
