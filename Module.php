6cc6b7548aee3fa83cac1dddaa6708a684a83414
sash04ek <a.ovcharov@gmail.com>
2020-12-29 12:48:10 +0300

    added Login method

--- Module.php
+++ Module.php
@@ -4,7 +4,7 @@
  * if commercial version of the product was purchased.
  * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
  */
- 
+
 namespace Aurora\Modules\AdminAuth;
 
 /**
@@ -28,10 +28,10 @@ class Module extends \Aurora\System\Module\AbstractModule
 		$this->subscribeEvent('CheckAccountExists', array($this, 'onCheckAccountExists'));
 		$this->subscribeEvent('System::RunEntry::before', array($this, 'onBeforeRunEntry'));
 	}
-	
+
 	/**
 	 * Return crypted password.
-	 * 
+	 *
 	 * @param string $Password
 	 * @return string
 	 */
@@ -40,11 +40,36 @@ class Module extends \Aurora\System\Module\AbstractModule
 		return crypt(trim($Password), \Aurora\System\Api::$sSalt);
 	}
 
+	public function Login($Login, $Password)
+	{
+		$mResult = false;
+		$oSettings =& \Aurora\System\Api::GetSettings();
+		if ($Login === $oSettings->AdminLogin)
+		{
+			if ($this->isClientIpInWhitelist())
+			{
+				$sAdminPassword = $oSettings->AdminPassword;
+				$bCorrectEmptyPass = empty($Password) && empty($sAdminPassword);
+				$bCorrectPass = $this->CryptPassword($Password) === $sAdminPassword;
+
+				if ($bCorrectEmptyPass || $bCorrectPass)
+				{
+					$mResult = [
+						'token' => 'admin',
+						'id' => '-1'
+					];
+				}
+			}
+		}
+
+		return $mResult;
+	}
+
 	/**
 	 * Checks if superadmin has specified login.
-	 * 
+	 *
 	 * @param string $sLogin Login for checking.
-	 * 
+	 *
 	 * @throws \Aurora\System\Exceptions\ApiException
 	 */
 	public function onCheckAccountExists($aArgs)
@@ -58,38 +83,16 @@ class Module extends \Aurora\System\Module\AbstractModule
 
 	/**
 	 * Tries to log in with specified credentials.
-	 * 
+	 *
 	 * @param array $aParams Parameters contain the required credentials.
 	 * @param array|mixed $mResult Parameter is passed by reference for further filling with result. Result is the array with data for authentication token.
 	 */
 	public function onLogin(&$aArgs, &$mResult)
 	{
 		$oSettings =&\Aurora\System\Api::GetSettings();
-		$sAdminPassword = $oSettings->AdminPassword;
-		
-		$bCorrectEmptyPass = empty($aArgs['Password']) && empty($sAdminPassword);
-		
-		$bCorrectPass = $this->cryptPassword($aArgs['Password']) === $oSettings->AdminPassword;
-		
 		if ($aArgs['Login'] === $oSettings->AdminLogin)
 		{
-			if ($bCorrectEmptyPass || $bCorrectPass)
-			{
-				if (!$this->isClientIpInWhitelist())
-				{
-					$mResult = false;
-					return true;
-				}
-
-				$mResult = array(
-					'token' => 'admin',
-					'id' => '-1'
-				);
-			}
-			else
-			{
-				$mResult = false;
-			}
+			$mResult = $this->Login($aArgs['Login'], $aArgs['Password']);
 			return true;
 		}
 	}
@@ -100,7 +103,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 
 		$aWhitelistIp = $this->getConfig('SuperadminWhitelistIp', []);
 		$ip = \Aurora\System\Utils::getClientIp();
-					
+
 		if (!empty($ip) && count($aWhitelistIp) > 0 && !in_array($ip, $aWhitelistIp))
 		{
 			$mResult = false;
@@ -112,7 +115,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 	public function onBeforeRunEntry(&$aArgs, &$mResult)
 	{
 		$oAuthenticatedUser = \Aurora\System\Api::getAuthenticatedUser();
-		if ($oAuthenticatedUser instanceof \Aurora\Modules\Core\Classes\User && 
+		if ($oAuthenticatedUser instanceof \Aurora\Modules\Core\Classes\User &&
 			$oAuthenticatedUser->Role === \Aurora\System\Enums\UserRole::SuperAdmin && !$this->isClientIpInWhitelist())
 		{
 			if (isset($aArgs['EntryName']) && strtolower($aArgs['EntryName']) === 'default')
@@ -122,16 +125,16 @@ class Module extends \Aurora\System\Module\AbstractModule
 			else
 			{
 				$mResult = \Aurora\System\Managers\Response::GetJsonFromObject(
-					'Json', 
+					'Json',
 					\Aurora\System\Managers\Response::ExceptionResponse(
-						'RunEntry', 
+						'RunEntry',
 						new \Aurora\System\Exceptions\ApiException(
 							\Aurora\System\Notifications::AccessDenied,
 							null,
 							$this->i18N('ERROR_USER_ACCESS_DENIED'),
-							[], 
+							[],
 							$this
-						)	
+						)
 					)
 				);
 				return true;
